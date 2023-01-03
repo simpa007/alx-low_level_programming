5d0ccc3f436059be96d79befec8a76149288cf9e
simpa007 <simpaimmey@gmail.com>
2023-01-03 00:54:22 -0800

    updates

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
- * main - Entry
- * Return: Alway0
+ * main - print a given phrase
+ * Return: 0 if exited properly, non-zero otherwise
  */
 int main(void)
-{ 
+{
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
